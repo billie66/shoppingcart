51af619b3c3ecbc1e2f446a1e8799b3de17bece0
Billie Zhang <billiecoder@gmail.com>
2012-07-23 17:14:46 +0800

    rails new shoppingcart

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoppingcart::Application.config.secret_token = 'ff54a4b98b87659f404d2b94529dcd79e5e0fa962fbd8cd968208d9254731397b7a617d3eb72987aadec2c30e2dd9ea507f7a5ba6e77aeba7bfa5db9221506e5'
